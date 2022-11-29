22cf0b3a121c0bab5d3910950253a6104ddac4fa
Jeff Handa <jeffrey.handa@liferay.com>
2022-11-29 12:01:05 -0500

    Adding full example code

--- src/main/java/com/liferay/commerce/demo/checkout/step/DemoCheckoutStep.java
+++ src/main/java/com/liferay/commerce/demo/checkout/step/DemoCheckoutStep.java
@@ -1,8 +1,14 @@
 package com.liferay.commerce.demo.checkout.step;
 
+import com.liferay.commerce.constants.CommerceCheckoutWebKeys;
+import com.liferay.commerce.constants.CommerceWebKeys;
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.commerce.util.BaseCommerceCheckoutStep;
 import com.liferay.commerce.util.CommerceCheckoutStep;
 import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.util.ParamUtil;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -11,6 +17,9 @@ import javax.portlet.ActionResponse;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author Jeff Handa
@@ -33,13 +42,34 @@ public class DemoCheckoutStep extends BaseCommerceCheckoutStep {
     }
 
     @Override
+    public boolean isActive(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        /*
+         * Add logic to determine if this step is going to be used during this checkout.  Some examples might be to
+         * include a custom checkout step based on the type of Order or type of Items in the order.  Another common
+         * use case is to limit custom checkout steps to a specific Channel.
+         */
+        return true;
+    }
+    @Override
     public void processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+        CommerceOrder commerceOrder = (CommerceOrder)actionRequest.getAttribute(CommerceCheckoutWebKeys.COMMERCE_ORDER);
+
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date requestedDeliveryDate = ParamUtil.getDate(actionRequest, "requestedDeliveryDate", df);
+
+        commerceOrder.setRequestedDeliveryDate(requestedDeliveryDate);
+        _commerceOrderLocalService.updateCommerceOrder(commerceOrder);
 
     }
 
     @Override
     public void render(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
+        CommerceContext commerceContext = (CommerceContext)httpServletRequest.getAttribute(CommerceWebKeys.COMMERCE_CONTEXT);
+        CommerceOrder commerceOrder = commerceContext.getCommerceOrder();
+        Date requestedDeliveryDate = commerceOrder.getRequestedDeliveryDate();
+        httpServletRequest.setAttribute("requestedDeliveryDate", requestedDeliveryDate);
         _jspRenderer.renderJSP(
                 _servletContext, httpServletRequest, httpServletResponse,
                 "/demo_checkout_step_form.jsp");
@@ -52,4 +82,7 @@ public class DemoCheckoutStep extends BaseCommerceCheckoutStep {
             target = "(osgi.web.symbolicname=com.liferay.commerce.demo.checkout.step)"
     )
     private ServletContext _servletContext;
+
+    @Reference
+    private CommerceOrderLocalService _commerceOrderLocalService;
 }
--- src/main/resources/META-INF/resources/demo_checkout_step_form.jsp
+++ src/main/resources/META-INF/resources/demo_checkout_step_form.jsp
@@ -1,3 +1,13 @@
+<%@ page import="com.liferay.commerce.model.CommerceOrder" %>
+<%@ page import="com.liferay.commerce.model.CommerceOrderItem" %>
+<%@ page import="com.liferay.commerce.model.CommerceAddress" %>
+<%@ page import="com.liferay.portal.kernel.util.Constants" %>
+<%@ page import="com.liferay.commerce.product.model.CPDefinition" %>
+<%@ page import="java.util.Date" %>
+<%@ page import="com.liferay.portal.kernel.util.CalendarFactoryUtil" %>
+<%@ page import="java.util.Calendar" %>
+<%@ page import="com.liferay.portal.kernel.util.HtmlUtil" %>
+<%@ page import="java.util.List" %>
 <%@ taglib prefix="aui" uri="http://liferay.com/tld/aui" %>
 <%@ taglib prefix="portlet" uri="http://java.sun.com/portlet_2_0" %>
 
@@ -14,4 +24,43 @@
 
 <portlet:defineObjects />
 
-<h1>Hello /dev/24!</h1>
+<%
+    Date requestedDeliveryDate = (Date)request.getAttribute("requestedDeliveryDate");
+
+    int requestedDeliveryDay = 0;
+    int requestedDeliveryMonth = -1;
+    int requestedDeliveryYear = 0;
+
+    if (requestedDeliveryDate != null) {
+        Calendar calendar = CalendarFactoryUtil.getCalendar(requestedDeliveryDate.getTime());
+        requestedDeliveryDay = calendar.get(Calendar.DAY_OF_MONTH);
+        requestedDeliveryMonth = calendar.get(Calendar.MONTH);
+        requestedDeliveryYear = calendar.get(Calendar.YEAR);
+    }
+
+%>
+
+<portlet:actionURL name="saveRequestedDeliveryDate" var="saveRequestedDeliveryDateActionURL"/>
+
+<aui:form action="<%= saveRequestedDeliveryDateActionURL %>" data-senna-off="true" method="post" name="fm">
+
+    <aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.UPDATE %>" />
+
+                <div class="form-group input-select-wrapper">
+
+                    <liferay-ui:input-date
+                            dayParam="requestedDeliveryDateDay"
+                            dayValue="<%= requestedDeliveryDay %>"
+                            disabled="<%= false %>"
+                            monthParam="requestedDeliveryDateMonth"
+                            monthValue="<%= requestedDeliveryMonth %>"
+                            name="requestedDeliveryDate"
+                            nullable="<%= true %>"
+                            showDisableCheckbox="<%= false %>"
+                            yearParam="requestedDeliveryDateYear"
+                            yearValue="<%= requestedDeliveryYear %>"
+                    />
+                </div>
+
+</aui:form>
+
